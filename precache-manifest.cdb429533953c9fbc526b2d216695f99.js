0c93e70c9cd0c72841cbbb863c03c4bc0688bf82
Juandlc <juniorelpapa09@hotmail.com>
2020-08-17 22:22:26 -0500

    Updates

---
+++ precache-manifest.cdb429533953c9fbc526b2d216695f99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c823a8fd89a235019b4622385df568be",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "df6f5a62d5a8cbca6c4f",
+    "url": "/covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "0ba7369070f032cded0b",
+    "url": "/covid19-tracker/static/css/main.fe94a663.chunk.css"
+  },
+  {
+    "revision": "df6f5a62d5a8cbca6c4f",
+    "url": "/covid19-tracker/static/js/2.c71aadd8.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid19-tracker/static/js/2.c71aadd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba7369070f032cded0b",
+    "url": "/covid19-tracker/static/js/main.bb735814.chunk.js"
+  },
+  {
+    "revision": "099559483f4deef958d1",
+    "url": "/covid19-tracker/static/js/runtime-main.853463f0.js"
+  }
+]);
